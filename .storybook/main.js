b62df64d538a50dc2302dc693a00d9e4c96726d5
highshot-rk <ivannosal22@gmail.com>
2021-07-03 22:09:48 -0400

    init

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,18 +1,7 @@
-// .storybook/main.js
-
 module.exports = {
-    stories: ['../components/**/*.stories.mdx', '../components/**/*.stories.@(js|jsx|ts|tsx)'],
-    addons: [
-        '@storybook/addon-postcss',
-        {
-            name: '@storybook/addon-postcss',
-            options: {
-            postcssLoaderOptions: {
-                implementation: require('postcss'),
-            },
-            },
-        },
-        '@storybook/addon-essentials',
-        '@storybook/addon-links'
-    ]
-  }
+  stories: ['../components/**/*.stories.mdx', '../components/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials'
+  ]
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,5 @@
 // Import the global style enabling tailwind classes
-import '../styles/globals.css'
+import '../src/index.css'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
