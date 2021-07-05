6029312ac95effe108331f411c5925b2aa5d7545
Shawn Inder <shawninder@gmail.com>
2021-07-05 12:15:04 -0400

    Rebase from master

--- components/form/Input.tsx
+++ components/form/Input.tsx
@@ -1,13 +1,34 @@
 import React, { FC, HTMLAttributes } from 'react'
+import { ErrorIcon } from "../svg/Error"
+import { ValidatingIcon } from "../svg/Validating"
+import { TickIcon } from "../svg/Tick"
 
 export interface InputProps extends HTMLAttributes<HTMLDivElement> {
-    error?: boolean
-    validating?: boolean
-    validated?: boolean
-    focused?: boolean
-    isSecure?: boolean
-    isNumber?: boolean
-    toggleSecure?: boolean
+    /**
+     * focussed type
+     */
+    isFocussed?: boolean
+    /**
+     * filled text
+     */
+    text?: string
+    /**
+     * error
+     */
+    isError?: boolean
+    /**
+     * checking...
+     */
+    isValidating?: boolean
+    /**
+     * success
+     */
+    isSuccessed?: boolean
+    /**
+     * disabled
+     */
+    isDisabled?: boolean
+
 }
 
 /**
@@ -15,12 +36,25 @@ export interface InputProps extends HTMLAttributes<HTMLDivElement> {
  */
 export const Input: FC<InputProps> = (props) => {
     return (
-        <div className="flex flex-col gap-y-1">
-            <div className="text-sm inter font-semibold">Label</div>
-            <div className="col-span-2 ...">
-                <input className={`text-base py-3 px-4 rounded-lg border border-gray-300 w-72 focus:outline-none focus:ring-2 focus:border-blue-400 focus:border-transparent shadow ${props.focused ? "outline-none ring-2 border-blue-400 border-transparent" : ""}`  } />
+        <div className="flex flex-col gap-y-1 w-72">
+            <div className={`text-sm font-inter font-semibold flex flex-row justify-between dark:text-gray-300 ${ props.isDisabled && "text-gray-500 dark:text-gray-600"}`}>
+                <p>Label</p>
+                { props.isError && <ErrorIcon />}
+                { props.isValidating && <ValidatingIcon />}
+                { props.isSuccessed && <TickIcon />}
             </div>
-            <div className="text-sm inter font-normal">Information</div>
+            <div className="col-span-2">
+                <input className={`dark:bg-gray-800 dark:text-white text-base py-3 px-4 rounded-lg border ${ !props.isDisabled && "hover:border-gray-300" }border-gray-200 dark:border-gray-800 w-72 focus:outline-none focus:ring-2 focus:border-input_focus focus:border-transparent shadow
+                ${props.isFocussed && "drop-shadow-sm outline-none ring-2 border-input_focus border-transparent dark:ring-offset-0"}
+                ${props.isError && "drop-shadow-sm outline-none ring-2 ring-red-600 border-transparent dark:ring-offset-0"}
+                ${props.isValidating && "drop-shadow-sm outline-none ring-2 ring-blue-600 border-transparent dark:ring-offset-0"}
+                ${props.isSuccessed && "drop-shadow-sm outline-none ring-2 ring-green-800 border-transparent dark:ring-offset-0"}` }
+                defaultValue={props.text}
+                disabled={props.isDisabled}
+                />
+            </div>
+            <div className={`text-sm font-inter font-normal dark:text-gray-300 ${ props.isDisabled && "text-gray-500 dark:text-gray-600"} `}>Information</div>
+            {props.isError && (<div className="text-sm font-normal text-red-700 font-inter">Error message</div>)}
         </div>
     )
 }
