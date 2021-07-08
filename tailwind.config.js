4f1ed3fc65164f9572163ccbb13026869a7ed3a2
Catherine <catherine.desrochers@gmail.com>
2021-07-08 18:36:37 -0400

    Removed old tailwind unused styles (these were bad practices)

--- tailwind.config.js
+++ tailwind.config.js
@@ -106,49 +106,7 @@ module.exports = {
           900: '#78350F',
         }
       },
-      textColor: theme => theme('colors'),
-      textColor: {
-        'button': {
-          'primary_dark': '#022631',
-          'hover': '#023846',
-          'default': '#25B6D0',
-          'dark': {
-            'hover': '#BDEFF9',
-            'default': '#7BE1EF',
-          }
-        }
-      },
-      borderColor: theme => ({
-        ...theme('colors'),
-        DEFAULT: theme('colors.gray.300', 'currentColor'),
-        'input_focus': '#63AFC0',
-        'secondary': '#ffed4a',
-        'danger': '#e3342f',
-      }),
-      backgroundColor: theme => ({
-        ...theme('colors'),
-        'button': {
-          'hover': '#0B6A7D',
-          'default': '#25B6D0',
-          'dark': {
-            'hover': '#9EE9F5',
-            'default': '#7BE1EF',
-          }
-        }
-       })
     },
-    boxShadow: {
-      sm: '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
-      DEFAULT: '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
-      md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
-      lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
-      xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
-      '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
-      '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
-      inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
-      none: 'none',
-      'input_focus': '0px 0px 10px #75B6C5'
-    }
   },
   variants: {
     extend: {
