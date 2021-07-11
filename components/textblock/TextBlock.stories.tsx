b97d99efdbac8b78cdee05fff6161c435235e5e2
highshot-rk <ivannosal22@gmail.com>
2021-07-12 00:20:44 +0900

    complete textblock

---
+++ components/textblock/TextBlock.stories.tsx
@@ -0,0 +1,22 @@
+import { TextBlock, TextBlockProps } from "./TextBlock"
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+export default {
+  title: 'Tjikko/TextBlock',
+  component: TextBlock,
+  argTypes: {
+    // type: { control: { type: "select", options: ["default", "header", "button", "special"] }},
+  },
+  parameters: {
+    componentSubtitle: 'The TextBlock'
+  }
+} as Meta
+
+const Template: Story<TextBlockProps> = (args) => <TextBlock {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+  className: 'w-80 h-52',
+  headerText: '01. HEADING',
+  paragraph: 'Lorem ipsum dolor sit amet, consectetur adipiscig elit ed do elusmod tempor incidedunt ut laborre et dolore magna aliqua'
+}
