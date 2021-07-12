848e4aa475de8e4854e0b302662662b5587b2efc
Catherine <catherine.desrochers@gmail.com>
2021-07-12 17:55:43 -0400

    Merge remote-tracking branch 'origin/master' into feature/logopizza
    1. Added the asset folder to Storybook
    https://storybook.js.org/docs/react/configure/images-and-assets
    
    2. We will use react plugin (react-fast-marquee) instead of the html marquee tag as html marquee tag is deprecated
    
    3. There was a problem with the images ratio that has been solved

--- components/logopizza/LogoPizza.stories.tsx
+++ components/logopizza/LogoPizza.stories.tsx
@@ -16,14 +16,15 @@ const Template: Story<LogoPizzaProps> = (args) => <LogoPizza {...args} />
 export const Default = Template.bind({})
 Default.args = {
     companyArr: [
-        { imgUrl: "", href: "#" },
-        { imgUrl: "", href: "#" },
-        { imgUrl: "", href: "#" },
-        { imgUrl: "", href: "#" },
-        { imgUrl: "", href: "#" },
-        { imgUrl: "", href: "#" },
-        { imgUrl: "", href: "#" },
-        { imgUrl: "", href: "#" },
+        { imgUrl: "./images/test_logo_01.png", href: "#" },
+        { imgUrl: "./images/test_logo_02.png", href: "#" },
+        { imgUrl: "./images/test_logo_03.png", href: "#" },
+        { imgUrl: "./images/test_logo_04.png", href: "#" },
+        { imgUrl: "./images/test_logo_05.png", href: "#" },
+        { imgUrl: "./images/test_logo_06.png", href: "#" },
+        { imgUrl: "./images/test_logo_07.png", href: "#" },
+        { imgUrl: "./images/test_logo_08.png", href: "#" },
+        //{ imgUrl: "../../assets/images/test_logo_09.png", href: "#" },
     ],
     mode: "Default"
 }
--- components/logopizza/LogoPizza.tsx
+++ components/logopizza/LogoPizza.tsx
@@ -1,10 +1,11 @@
 import React, { FC, HTMLAttributes, useState, useEffect } from 'react'
+import Marquee from "react-fast-marquee";
 
 export interface LogoPizzaInterface {
     imgUrl: string;
     href?: string;
 }
-
+    
 export interface LogoPizzaProps extends HTMLAttributes<HTMLDivElement> {
     /**
      * speech to display on LogoPizza
@@ -31,21 +32,23 @@ export const LogoPizza: FC<LogoPizzaProps> = ({
             {...props}
         >
             <h3 className={`${ mode == "Compact" ? "text-2xl" : "text-4xl"} text-center mb-16`}>Trusted by</h3>
-            <marquee>
-                <div className={`flex flex-nowrap w-max`}>
+            <Marquee
+                gradientWidth=  "0"
+            >
+                <div className={`flex flex-nowrap w-max  space-x-14 lg:space-x-32 md:space-x-16`}>
                 { companyArr.map((company, id) => (
                     <div
                         key={id}
-                        className={ mode == "Default" ? "w-14 h-14 lg:w-24 lg:h-24 md:w-20 md:h-20 sm:w-14 sm:h-14 mx-14 lg:mx-32 md:mx-16 sm:mx-14" : "w-14 h-14 lg:w-16 lg:h-16 md:w-12 md:h-12 md:w-14 md:h-14 sm:w-14 sm:h-14 mx-14 lg:mx-32 md:mx-16 sm:mx-14"}
+                        className={ mode == "Default" ? "h-14 lg:h-24 md:h-20" : "h-14 lg:h-16 md:h-12"}
                     >
                         { (company.imgUrl && company.imgUrl !== "") ? 
-                            <img className="w-full h-full" src={company.imgUrl ? company.imgUrl : `https://i.pravatar.cc/${Math.ceil(1000 * Math.random())}`}></img>
+                            <img className="w-auto h-full" src={company.imgUrl ? company.imgUrl : `https://i.pravatar.cc/${Math.ceil(1000 * Math.random())}`}></img>
                             : <div className="w-full h-full bg-black"></div>
                         }
                     </div>
                 )) }
                 </div>
-            </marquee>
+            </Marquee>
         </div>
     )
 }
