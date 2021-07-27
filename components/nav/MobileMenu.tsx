f88a8359be8b4282a650a76aa41f75c6da329e72
Catherine <catherine.desrochers@gmail.com>
2021-07-26 22:29:14 -0400

    A better sitenav
    - Normalized fonts
    - Better responsive behaviour
    - Added a divider component
    - Removed useless classes
    - Added the ListNav component  inside the mobile nav
    - Added our own icons for the mobile nav
    - Replaced black/transwhite by more relevant names Opaque/Transparent
    - Transparent with no longer backgrounds

--- components/nav/MobileMenu.tsx
+++ components/nav/MobileMenu.tsx
@@ -1,6 +1,9 @@
 import React, {FC, HTMLAttributes} from 'react'
 import {SiteNavProps} from './SiteNav'
 import {NavItem} from './NavItem'
+import {Divider} from './Divider'
+import {Button} from '../Button'
+import {ListNav} from './ListNav'
 
 /**
  * Primary UI component for user interaction
@@ -14,19 +17,18 @@ export const MobileMenu: FC<SiteNavProps> = ({
 }) => {
   return (
     <div
-      className={`bg-gray-900 ${styles === 'transWhite' && 'bg-opacity-10'}`}
+      className={`bg-gray-900 p-4`}
     >
-      <div className='flex justify-between p-5 p-6'>
-        <div>
-          <a
-            href={demoUrl}
-            className='py-3.5 px-3.5 uppercase bg-brand-400 rounded-lg tracking-wider leading-3 text-sm font-semibold'
-          >
-            {demoButtonText}
-          </a>
+      <div className='flex justify-between p-2'>
+        <div className="dark">
+          <Button
+            text={demoButtonText}
+            url={demoUrl}
+            type="primary"
+            icon='none'
+          />
         </div>
         <NavItem
-          className='mr-7'
           styles='default/white'
           caption={
             languageList.current ? languageList.current : 'En'
@@ -34,12 +36,16 @@ export const MobileMenu: FC<SiteNavProps> = ({
           submenu={languageList.submenu}
         />
       </div>
-      <hr className='mx-2 bg-opacity-10 mt-6 text-gray-400' />
+      <div className="dark">
+        <Divider className="my-8" />
+      </div>
 
       {menuData.map((menu, menuIndex) => {
         return (
-          <div key={menuIndex}>
-            <div className='font-borda uppercase text-gray-50 text-lg tracking-widest pl-6 pt-6 '>
+          <div 
+            className='dark p-2'
+            key={menuIndex}>
+            <div className='fontStyle-xl text-gray-50 mb-6'>
               {menu.submenu == null ? (
                 <a href={menu.captionLink}>{menu.caption}</a>
               ) : (
@@ -50,38 +56,16 @@ export const MobileMenu: FC<SiteNavProps> = ({
               {menu.submenu === null ? (
                 null
               ) : (
-                <div className='grid grid-cols-2'>
-                  {menu.submenu.map((group, groupIndex) => {
-                    return (
-                      <div key={groupIndex}>
-                        {group.groupCaption && (
-                          <div className='text-gray-100 font-semibold tracking-wider text-sm pl-6 pt-5'>
-                            {group.groupCaption}
-                          </div>
-                        )}
-                        {group.groups.map((item, itemIndex) => {
-                          return (
-                            <div
-                              className='px-6 pt-5 text-gray-100'
-                              key={itemIndex}
-                            >
-                              <a
-                                className={item.type === 'button' ? 'text-brand-300 hover:text-brand-600' : ''}
-                                href={item.url}
-                              >
-                                {item.name}
-                              </a>
-                            </div>
-                          )
-                        })}
-                      </div>
-                    )
-                  })}
+                <div>
+                  <ListNav 
+                    styles="flat"
+                    linkList= {menu.submenu}
+                  />
                 </div>
               )}
             </div>
             {menuData.length - 1 > menuIndex && (
-              <hr className='mx-2 bg-opacity-10 mt-6 text-gray-400' />
+              <Divider className="my-8" />
             )}
           </div>
         )
--- components/nav/PopUpNavItem.tsx
+++ components/nav/PopUpNavItem.tsx
@@ -36,7 +36,7 @@ export const PopUpNavItem: FC<PopUpNavItemProps> = ({
   const classes = ['flex', 'items-center']
   switch (type) {
     case 'special':
-      classes.push('px-3', 'py-2.5', 'fontStyle-bold', 'text-gray-100')
+      classes.push('py-2.5', 'fontStyle-base', 'text-gray-100')
       if (isActive) {
         classes.push('text-brand-300', 'dark:text-brand-300')
       } else {
@@ -45,10 +45,10 @@ export const PopUpNavItem: FC<PopUpNavItemProps> = ({
 
       break
     case 'header':
-      classes.push('uppercase', 'px-3', 'py-2.5', 'font-semibold', 'tracking-wider', 'text-gray-800', 'text-sm', 'dark:text-gray-100')
+      classes.push('fontStyle-sm', 'uppercase', 'strong', 'py-2.5', 'text-gray-800', 'dark:text-gray-100')
       break
     case 'button':
-      classes.push('px-3', 'py-3.5', 'tracking-wider', 'text-xs', 'font-semibold')
+      classes.push('py-3.5', 'fontStyle-xs', 'uppercase', 'strong')
       if (isActive) {
         classes.push('text-brand-700', 'dark:text-brand-100')
       } else {
@@ -57,7 +57,7 @@ export const PopUpNavItem: FC<PopUpNavItemProps> = ({
       break
     case 'default':
     default:
-      classes.push('px-3', 'py-2.5', 'text-sm')
+      classes.push('py-2.5', 'fontStyle-sm')
       if (isActive) {
         classes.push('text-brand-600', 'dark:text-brand-300')
       } else {
@@ -68,7 +68,6 @@ export const PopUpNavItem: FC<PopUpNavItemProps> = ({
   return (
     <div
       className={classes.join(' ')}
-      style={{width: 'fit-content'}}
     >
       <a href={href}>
         {caption}
