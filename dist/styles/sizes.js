49ae5fd2885abdbcc5c284d2124086c03e05d071
Catherine <catherine.desrochers@gmail.com>
2021-08-03 10:04:47 -0400

    Last little update
    - Added additional settings that I was implemented in the backend tailwind file
    - Included styles in the build script

--- dist/styles/sizes.js
+++ dist/styles/sizes.js
@@ -6,9 +6,35 @@ Object.defineProperty(exports, "__esModule", {
 exports.sizeOptions = void 0;
 var sizeOptions = {
   height: {
-    "90vh": "90vh",
-    "568": "568px"
+    "1/3vh": "33.333vh",
+    "2/3vh": "66.666vh",
+    "1/4vh": "25vh",
+    "3/4vh": "75vh",
+    "1/5vh": "20vh",
+    "2/5vh": "40vh",
+    "3/5vh": "60vh",
+    "4/5vh": "80vh",
+    "1/2vh": "50vh",
+    "100vh": "100vh",
+    "1/3vw": "33.333vw",
+    "2/3vw": "66.666vw",
+    "1/4vw": "25vw",
+    "3/4vw": "75vw",
+    "1/5vw": "20vw",
+    "2/5vw": "40vw",
+    "3/5vw": "60vw",
+    "4/5vw": "80vw",
+    "1/2vw": "50vw",
+    "100vw": "100vw"
+  },
+  screens: {
+    'xxs': '375px',
+    'xs': '414px',
+    'sm': '640px',
+    'md': '768px',
+    'lg': '1024px',
+    'xl': '1280px',
+    '2xl': '1536px'
   }
 };
 exports.sizeOptions = sizeOptions;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3N0eWxlcy9zaXplcy50c3giXSwibmFtZXMiOlsic2l6ZU9wdGlvbnMiLCJoZWlnaHQiXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFPLElBQUlBLFdBQVcsR0FBRztBQUN2QkMsRUFBQUEsTUFBTSxFQUFFO0FBQ04sWUFBUSxNQURGO0FBRU4sV0FBTztBQUZEO0FBRGUsQ0FBbEIiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgbGV0IHNpemVPcHRpb25zID0ge1xuICBoZWlnaHQ6IHtcbiAgICBcIjkwdmhcIjogXCI5MHZoXCIsXG4gICAgXCI1NjhcIjogXCI1NjhweFwiLFxuICB9XG59OyJdfQ==
