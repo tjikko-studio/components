db3c913b7703f1921c50e69f995cad724fe34c56
Shawn Inder <shawninder@gmail.com>
2021-08-09 19:19:35 -0400

    fix `useBreakpoint`

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
 const plugin = require('tailwindcss/plugin')
 const styles = require('./styles')
 
+/** This guy prevents `useBreakpoint` breaking, no idea why or how */
 /** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 
-module.exports = {
+const config = {
   plugins: [
-    plugin.withOptions(() => {},() => {
-      return styles
-    }),
     require('@tailwindcss/forms'),
-    plugin(require('./fontStyles'))
+    plugin(require('./fontStyles')),
+    plugin.withOptions(
+      function () {},
+      function () {
+        return styles
+      }
+    )
   ]
 }
+
+module.exports = config
