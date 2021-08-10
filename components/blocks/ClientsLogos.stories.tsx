b6eb232df360d709c3ca89d87e735c0f0a96709c
Catherine <catherine.desrochers@gmail.com>
2021-08-10 19:14:56 -0400

    Clients Logo Cleanup
    - Updated content structure
    - Removes titles
    - renamed properties

--- components/blocks/ClientsLogos.stories.tsx
+++ components/blocks/ClientsLogos.stories.tsx
@@ -15,16 +15,52 @@ const Template: Story<ClientsLogosProps> = (args) => <ClientsLogos {...args} />
 
 export const Default = Template.bind({})
 Default.args = {
-  companyArr: [
-    {imgUrl: './images/test_logo_01.png', href: '#'},
-    {imgUrl: './images/test_logo_02.png', href: '#'},
-    {imgUrl: './images/test_logo_03.png', href: '#'},
-    {imgUrl: './images/test_logo_04.png', href: '#'},
-    {imgUrl: './images/test_logo_05.png', href: '#'},
-    {imgUrl: './images/test_logo_06.png', href: '#'},
-    {imgUrl: './images/test_logo_07.png', href: '#'},
-    {imgUrl: './images/test_logo_08.png', href: '#'},
-    { imgUrl: './images/test_logo_09.png', href: '#' }
+  content: [
+    {
+      image: [{
+        url: './images/test_logo_01.png'
+      }],
+      company: 'Company 01'},
+    {
+      image: [{
+        url: './images/test_logo_02.png'
+      }],
+      company: 'Company 02'},
+    {
+      image: [{
+        url: './images/test_logo_03.png'
+      }],
+      company: 'Company 03'},
+    {
+      image: [{
+        url: './images/test_logo_04.png'
+      }],
+      company: 'Company 04'},
+    {
+      image: [{
+        url: './images/test_logo_05.png'
+      }],
+      company: 'Company 05'},
+    {
+      image: [{
+        url: './images/test_logo_06.png'
+      }],
+      company: 'Company 06'},
+    {
+      image: [{
+        url: './images/test_logo_07.png'
+      }],
+      company: 'Company 07'},
+    {
+      image: [{
+        url: './images/test_logo_08.png'
+      }],
+      company: 'Company 08'},
+    {
+      image: [{
+        url: './images/test_logo_09.png'
+      }],
+      company: 'Company 09' }
   ],
   size: 'Default'
 }
--- components/blocks/ClientsLogos.tsx
+++ components/blocks/ClientsLogos.tsx
@@ -1,16 +1,11 @@
-import React, {FC, HTMLAttributes, useState, useEffect} from 'react'
+import React, { FC, HTMLAttributes } from 'react'
 import Marquee from 'react-fast-marquee'
-
-export interface ClientsLogosInterface {
-  imgUrl: string
-  href?: string
-}
 
 export interface ClientsLogosProps extends HTMLAttributes<HTMLDivElement> {
   /**
    * speech to display on ClientsLogos
    */
-  companyArr?: ClientsLogosInterface[]
+  content?: {}
   /**
    * Size : 'Default' | 'Compact'
    */
@@ -21,27 +16,28 @@ export interface ClientsLogosProps extends HTMLAttributes<HTMLDivElement> {
  * Primary UI component for user interaction
  */
 export const ClientsLogos: FC<ClientsLogosProps> = ({
-  companyArr = [],
-  size = 'Default'
+  content,
+  size,
 }) => {
-
+  
+  {/* <Marquee gradientWidth='0' > */}
   return (
-    <Marquee
-      gradientWidth='0'
-    >
       <div className={`flex flex-nowrap w-max  space-x-14 lg:space-x-32 md:space-x-16`}>
-        {companyArr.map((company, id) => (
-          <div
-            key={id}
-            className={size === 'Default' ? 'h-14 lg:h-24 md:h-20' : 'h-14 lg:h-16 md:h-12'}
-          >
-            {(company.imgUrl && company.imgUrl !== '') ?
-              <img className='w-auto h-full' src={company.imgUrl ? company.imgUrl : `https://i.pravatar.cc/${Math.ceil(1000 * Math.random())}`}></img>
-              : <div className='w-full h-full bg-black'></div>
-            }
-          </div>
-        ))}
+        { content.map((company, id) => {
+          let image = company.image[0]
+          return (
+            <div
+              key={id}
+              className={size === 'Default' ? 'h-14 lg:h-24 md:h-20' : 'h-14 lg:h-16 md:h-12'}
+            >
+              {(image.url) &&
+                <img className='w-auto h-full' src={image.url ? image.url : `https://i.pravatar.cc/${Math.ceil(1000 * Math.random())}`}/>
+              }
+            </div>
+          )
+          })
+        }
       </div>
-    </Marquee>
   )
+  {/* </Marquee> */}
 }
