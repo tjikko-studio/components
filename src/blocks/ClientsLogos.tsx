74a8f9f8b977f9e5a336823e8c410d4fefa326f4
Shawn Inder <shawninder@gmail.com>
2021-08-20 23:04:08 -0400

    `<marquee>` has been soft-banned from the web for years…

--- src/blocks/ClientsLogos.tsx
+++ src/blocks/ClientsLogos.tsx
@@ -28,7 +28,7 @@ export const ClientsLogos: FC<ClientsLogosProps> = ({
   content = []
 }) => {
   return (
-    <marquee>
+    <div>
     {/* <Marquee gradientWidth='0'> */}
       <div className={`flex flex-nowrap w-max  space-x-14 lg:space-x-32 md:space-x-16`}>
         {content.map(({image, company}) => {
@@ -44,6 +44,6 @@ export const ClientsLogos: FC<ClientsLogosProps> = ({
         }
       </div>
     {/* </Marquee> */}
-    </marquee>
+    </div>
   )
 }
