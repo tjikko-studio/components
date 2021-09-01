5b89d0dce5c351e86cd45948279a391642654cdc
Shawn Inder <shawninder@gmail.com>
2021-09-01 19:53:32 -0400

    Tell tailwind what to purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ const styles = require('./styles')
 const applyFontConfig = require('./applyFontConfig')
 
 module.exports = {
+  purge: {
+    content: [
+      './src/**/*.tsx',
+      './styles/**/*.tsx',
+      './index.js',
+      './styles.js'
+    ]
+  },
   plugins: [
     require("@tailwindcss/forms")({
       strategy: 'class',
