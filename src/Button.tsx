ac8d3ebcb3863f0cd9735b188a92fd2be2805eb0
Kaushalkumar <60640854+kr396@users.noreply.github.com>
2021-09-29 22:13:37 +0530

    fixed ui issue

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,166 +1,157 @@
-// eslint-disable-next-line no-use-before-define
-import React, {FC, HTMLAttributes} from 'react'
+import React, { FC, HTMLAttributes } from "react";
 
 export interface ButtonProps extends HTMLAttributes<HTMLDivElement> {
   /**
    * button type
    */
-  type?: 'primary' | 'secondary' | 'tertiary'
+  type?: "primary" | "secondary" | "tertiary";
   /**
    * text to display on button
    */
-  label?: string
+  label?: string;
 
   /**
    * icon position
    */
-  iconPos?: 'right' | 'left'
+  iconPos?: "right" | "left";
 
   /**
    * icon image
    */
-  icon?: string
+  icon?: string;
 
   /**
    * icon size
    */
-  size?: 'default' | 'large' | 'small'
+  size?: "default" | "large" | "small";
 
   /**
    * button link
    */
-  link?: string
+  link?: string;
 
   /**
-   * className override
+   * className modifier that will add custom classes if needed (margin, padding, direction, etc.)
    */
-  className?: string
+  className?: string;
 
   /**
-   * apply the dark theme whether in dark or light mode
+   * force dark mode (This will bypass dark mode and apply the dark mode on the component even in light mode
    */
-  forceDark?: boolean
+  forceDark?: boolean;
 }
 
 /**
  * Primary UI component for user interaction
  */
 export const Button: FC<ButtonProps> = ({
-  type = 'primary',
-  label = 'Button',
-  size = 'default',
-  icon = '',
-  iconPos = 'left',
-  link = '',
-  className = '',
-  forceDark = false
+  type = "primary",
+  label = "Button",
+  size = "default",
+  icon = "",
+  iconPos = "left",
+  link = "",
+  className = "",
+  forceDark = false,
 }) => {
-  const buttonClasses = ['inline-flex items-center space-x-3']
-  const contentClasses = ['']
-  const styles = {
-    primary: (isForceDark:boolean) => {
-      const newClasses = ['rounded-lg']
-      if (isForceDark) {
-        newClasses.push('bg-primary-400', 'hover:bg-primary-200')
-        newClasses.push('text-primary-900')
+  const buttonClasses = ["inline-flex items-center"];
+  const lableClasses = [""];
+  const iconClasses = ["max-h-full"];
+
+  switch (type) {
+    case "primary":
+      buttonClasses.push("rounded-lg");
+      if (forceDark) {
+        buttonClasses.push("bg-primary-400 hover:bg-primary-200");
+        buttonClasses.push("text-primary-900");
       } else {
-        newClasses.push(
-          'bg-primary-600',
-          'hover:bg-primary-700',
-          'dark:bg-primary-400',
-          'dark:hover:bg-primary-200'
-        )
-        newClasses.push('text-white', 'dark:text-primary-900')
+        buttonClasses.push(
+          "bg-primary-600 hover:bg-primary-700 dark:bg-primary-400 dark:hover:bg-primary-200"
+        );
+        buttonClasses.push("text-white dark:text-primary-900");
       }
-      return newClasses
-    },
-    secondary: (isForceDark:boolean) => {
-      const newClasses = ['bg-none rounded-lg border']
-      if (isForceDark) {
-        newClasses.push('border-primary-400 hover:border-primary-200')
-        newClasses.push('text-primary-400 hover:text-primary-200')
+      break;
+    case "secondary":
+      buttonClasses.push("bg-none rounded-lg border");
+      if (forceDark) {
+        buttonClasses.push("border-primary-400 hover:border-primary-200");
+        buttonClasses.push("text-primary-400 hover:text-primary-200");
       } else {
-        newClasses.push(
-          'border-primary-600',
-          'hover:border-primary-800',
-          'dark:border-primary-300',
-          'dark:hover:border-primary-100'
-        )
-        newClasses.push(
-          'text-primary-600',
-          'hover:text-primary-800',
-          'dark:text-primary-300',
-          'dark:hover:text-primary-100'
-        )
+        buttonClasses.push(
+          "border-primary-600 hover:border-primary-800 dark:border-primary-300  dark:hover:border-primary-100"
+        );
+        buttonClasses.push(
+          "text-primary-600 hover:text-primary-800 dark:text-primary-300 dark:hover:text-primary-100"
+        );
       }
-      return newClasses
-    },
-    tertiary: (isForceDark:boolean) => {
-      const newClasses = ['bg-none']
-      if (isForceDark) {
-        newClasses.push('text-primary-400 hover:text-primary-200')
+      break;
+    case "tertiary":
+      buttonClasses.push("bg-none");
+      if (forceDark) {
+        buttonClasses.push("text-primary-400 hover:text-primary-200");
       } else {
-        newClasses.push(
-          'text-primary-600',
-          'hover:text-primary-700',
-          'dark:text-primary-400',
-          'dark:hover:text-primary-200'
-        )
+        buttonClasses.push(
+          "text-primary-600 hover:text-primary-700 dark:text-primary-400 dark:hover:text-primary-200"
+        );
       }
-      return newClasses
-    }
+      break;
   }
-  buttonClasses.push(styles[type](forceDark).join(' '))
   switch (size) {
-  case 'small':
-    buttonClasses.push('h-8 max-h-8 py-2.5')
-    contentClasses.push('fontStyle-button-sm')
-    if (type !== 'tertiary') {
-      buttonClasses.push('px-3.5')
-    }
-    break
-  case 'large':
-    buttonClasses.push('h-12 max-h-12 py-4')
-    if (type !== 'tertiary') {
-      buttonClasses.push('px-5')
-    }
-    contentClasses.push('fontStyle-button-lg')
-    break
-  case 'default':
-  default:
-    buttonClasses.push('h-10 max-h-10 py-3.5')
-    if (type !== 'tertiary') {
-      buttonClasses.push('px-4')
-    }
-    contentClasses.push('fontStyle-button-base')
+    case "small":
+      buttonClasses.push("h-8 max-h-8 py-2");
+      lableClasses.push("fontStyle-button-sm");
+      type !== "tertiary" && buttonClasses.push("px-2.5");
+      icon && lableClasses.push(iconPos === "left" ? "ml-2" : "mr-2");
+
+      break;
+    case "large":
+      buttonClasses.push("h-12 max-h-12 py-3");
+      type !== "tertiary" && buttonClasses.push("px-5");
+      lableClasses.push("fontStyle-button-lg");
+      icon && lableClasses.push(iconPos === "left" ? "ml-4" : "mr-4");
+      break;
+    case "default":
+    default:
+      buttonClasses.push("h-10 max-h-10 py-2.5");
+      type !== "tertiary" && buttonClasses.push("px-4");
+      lableClasses.push("fontStyle-button-base");
+      icon && lableClasses.push(iconPos === "left" ? "ml-3" : "mr-4");
   }
-  buttonClasses.push(`${className}`)
-  const buttonClassesJoined = buttonClasses.join(' ')
+  buttonClasses.push(`${className}`);
+  const buttonClassesJoined = buttonClasses.join(" ");
+  const iconClassesJoined = iconClasses.join(" ");
   const Content = () => {
-    if (label) {
+    if (label && icon) {
       return (
-        <span
-          className={contentClasses.join(' ')}
-        >{label}</span>
-      )
+        <>
+          {iconPos === "left" && (
+            <img src={icon} className={iconClassesJoined} />
+          )}
+          <span className={lableClasses.join(" ")}>{label}</span>
+          {iconPos === "right" && (
+            <img src={icon} className={iconClassesJoined} />
+          )}
+        </>
+      );
     }
-    return null
-  }
+    if (label) {
+      return <span className={lableClasses.join(" ")}>{label}</span>;
+    }
+    if (icon) {
+      return <img src={icon} className={iconClassesJoined} />;
+    }
+  };
   if (link) {
     return (
-      <a
-        href={link}
-        className={buttonClassesJoined}
-      >
+      <a href={link} className={buttonClassesJoined}>
         <Content />
       </a>
-    )
+    );
+  } else {
+    return (
+      <button className={buttonClassesJoined}>
+        <Content />
+      </button>
+    );
   }
-  return (
-    <button
-      className={buttonClassesJoined}
-    >
-      <Content />
-    </button>
-  )
-}
+};
