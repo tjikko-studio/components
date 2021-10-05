a0787fa24d816f1df39fe4e95a9ca83610e4845b
Catherine <catherine.desrochers@gmail.com>
2021-10-05 13:48:17 -0400

    Added content placeholder for Hero, Sections & Form

--- src/collections/Form.tsx
+++ src/collections/Form.tsx
@@ -24,54 +24,65 @@ export interface FormProps extends HTMLAttributes<HTMLElement> {
 
 export const Form: FC<FormProps> = ({width = 'full', content = [], templatesContent = {}}) => {
   const toComponent = getComponent(templatesContent)
+  const PlaceholderContent = () => {
+    return <div>No content yet</div>
+  }
   return (
     <form className="grid gap-4">
-      {content.map(({columns}) => {
-        return (
-          <section
-            key={JSON.stringify(columns)}
-            className={'sm:grid sm:gap-x-4'}
-            style={{
-              gridTemplateColumns: 'repeat(12, minmax(0, auto))',
-              gridTemplateRows: 'repeat(4, minmax(0, auto))',
-              gridTemplateAreas: `
-"  label-0   label-1   label-2   label-3   label-4   label-5   label-6   label-7   label-8   label-9   label-10   label-11"
-"control-0 control-1 control-2 control-3 control-4 control-5 control-6 control-7 control-8 control-9 control-10 control-11"
-"   info-0    info-1    info-2    info-3    info-4    info-5    info-6    info-7    info-8    info-9    info-10    info-11"
-"  error-0   error-1   error-2   error-3   error-4   error-5   error-6   error-7   error-8   error-9   error-10   error-11"
-              `
-            }}
-          >
-            {columns.reduce((acc, column, columnIndex) => {
-              const newAcc = acc.concat(
-                column.blocks.map((block) => {
-                  return toComponent(block, {
-                    Input: (baseProps) => {
-                      return {
-                        className: cn(baseProps.className, 'w-full'),
-                        columnIndex
-                      }
-                    },
-                    TextArea: (baseProps) => {
-                      return {
-                        className: cn(baseProps.className, 'w-full'),
-                        columnIndex
-                      }
-                    },
-                    ButtonsGroup: (baseProps) => {
-                      return {
-                        className: cn(baseProps.className, 'w-full'),
-                        columnIndex
-                      }
-                    }
-                  })
-                })
-              )
-              return newAcc
-            }, [])}
-          </section>
-        )
-      })}
+      {content.length >= 1 ? (
+        content.map(({columns}) => {
+          return (
+            <section
+              key={JSON.stringify(columns)}
+              className={'sm:grid sm:gap-x-4'}
+              style={{
+                gridTemplateColumns: 'repeat(12, minmax(0, auto))',
+                gridTemplateRows: 'repeat(4, minmax(0, auto))',
+                gridTemplateAreas: `
+  "  label-0   label-1   label-2   label-3   label-4   label-5   label-6   label-7   label-8   label-9   label-10   label-11"
+  "control-0 control-1 control-2 control-3 control-4 control-5 control-6 control-7 control-8 control-9 control-10 control-11"
+  "   info-0    info-1    info-2    info-3    info-4    info-5    info-6    info-7    info-8    info-9    info-10    info-11"
+  "  error-0   error-1   error-2   error-3   error-4   error-5   error-6   error-7   error-8   error-9   error-10   error-11"
+                `
+              }}
+            >
+              {columns[0].blocks.length >= 1 ? (
+                columns.reduce((acc, column, columnIndex) => {
+                  const newAcc = acc.concat(
+                    column.blocks.map((block) => {
+                      return toComponent(block, {
+                        Input: (baseProps) => {
+                          return {
+                            className: cn(baseProps.className, 'w-full'),
+                            columnIndex
+                          }
+                        },
+                        TextArea: (baseProps) => {
+                          return {
+                            className: cn(baseProps.className, 'w-full'),
+                            columnIndex
+                          }
+                        },
+                        ButtonsGroup: (baseProps) => {
+                          return {
+                            className: cn(baseProps.className, 'w-full'),
+                            columnIndex
+                          }
+                        }
+                      })
+                    })
+                  )
+                  return newAcc
+                }, [])
+              ) : (
+                <PlaceholderContent />
+              )}
+            </section>
+          )
+        })
+      ) : (
+        <PlaceholderContent />
+      )}
     </form>
   )
 }
