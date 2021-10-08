04450f4fd262a12f695d8f2ecb61c1654f94b9de
Catherine <catherine.desrochers@gmail.com>
2021-10-08 10:58:00 -0400

    Added empty states to the logo component & nav builder

--- src/nav/Footer.tsx
+++ src/nav/Footer.tsx
@@ -3,7 +3,7 @@ import cn from 'classnames'
 
 import getWidth from '../../utilities/getWidth'
 import {Button} from '../Button'
-import {ImageProps, Media} from '../parts/Media'
+import {Media} from '../parts/Media'
 import {NavItem} from './NavItem'
 
 import {MenuItemType, MenuType} from '../../shared/types'
@@ -67,121 +67,128 @@ export const Footer: FC<FooterProps> = ({menuData = [], locales = null}) => {
               border
             ])}
           >
-            {columns.length &&
-              columns.map(({width, blocks, id: columnId}) => {
-                return (
-                  <div key={columnId || JSON.stringify(blocks)} className={`lg:col-span-${getWidth(width)} h-full`}>
-                    {blocks.length >= 1 &&
-                      blocks.map(({content, layout, rtl, id: blockId}) => {
-                        const contentLayout =
-                          layout === 'horizontal'
-                            ? ['flex', 'lg:mt-0', 'items-center', 'h-full']
-                            : ['lg:flex', 'items-start', 'flex-col', 'first:mt-0', 'lg:mt-0', 'space-y-4']
-                        const justify = rtl ? 'lg:justify-end' : ''
-                        const contentLength = content.length
-                        return (
-                          <div
-                            key={blockId || JSON.stringify(content)}
-                            className={cn([
-                              ...contentLayout,
-                              `lg:col-span-${getWidth(width)}`,
-                              layout === 'horizontal' && 'flex flex-end h-full',
-                              justify
-                            ])}
-                          >
-                            {content.length &&
-                              content.map(({label, link, type, content: innerContent, dataSource, id: contentId}, idx) => {
-                                return (
-                                  <div key={contentId || JSON.stringify(innerContent)}>
-                                    {(() => {
-                                      switch (type) {
-                                        case 'NavigationDropdownChild':
-                                          return (
-                                            <div
-                                              className={cn(['dark', 'flex', 'flex-col', 'space-y-4'])}
-                                              role={'navigation'}
-                                              aria-label={label}
-                                            >
-                                              {label && <div className={cn(['fontStyle-xs', 'uppercase', 'text-gray-300'])}>{label}</div>}
-                                              {innerContent.map(({label: innerLabel, link: innerLink}) => {
+            {columns.length
+              ? columns.map(({width, blocks, id: columnId}) => {
+                  return (
+                    <div key={columnId || JSON.stringify(blocks)} className={`lg:col-span-${getWidth(width)} h-full space-y-12`}>
+                      {blocks.length >= 1 &&
+                        blocks.map(({content, layout, rtl, id: blockId}) => {
+                          const contentLayout =
+                            layout === 'horizontal'
+                              ? ['flex', 'lg:mt-0', 'items-center', 'h-full']
+                              : ['lg:flex', 'items-start', 'flex-col', 'first:mt-0', 'lg:mt-0', 'space-y-4']
+                          const justify = rtl ? 'lg:justify-end' : ''
+                          const contentLength = content.length
+                          return (
+                            <div
+                              key={blockId || JSON.stringify(content)}
+                              className={cn([
+                                ...contentLayout,
+                                `lg:col-span-${getWidth(width)}`,
+                                layout === 'horizontal' && 'flex flex-end h-full',
+                                justify
+                              ])}
+                            >
+                              {content.length
+                                ? content.map(({label, link, type, content: innerContent, dataSource, id: contentId}, idx) => {
+                                    return (
+                                      <div key={contentId || JSON.stringify(innerContent)}>
+                                        {(() => {
+                                          switch (type) {
+                                            case 'NavigationDropdownChild':
+                                              return (
+                                                <div
+                                                  className={cn(['dark', 'flex', 'flex-col', 'space-y-4'])}
+                                                  role={'navigation'}
+                                                  aria-label={label}
+                                                >
+                                                  {label && (
+                                                    <div className={cn(['fontStyle-xs', 'uppercase', 'text-gray-300'])}>{label}</div>
+                                                  )}
+                                                  {innerContent.map(({label: innerLabel, link: innerLink}) => {
+                                                    return (
+                                                      <a key={`${innerLabel}${innerLink}`} href={innerLink} className="fontStyle-sm">
+                                                        {innerLabel}
+                                                      </a>
+                                                    )
+                                                  })}
+                                                </div>
+                                              )
+                                            case 'link': {
+                                              const Alink = () => {
                                                 return (
-                                                  <a key={`${innerLabel}${innerLink}`} href={innerLink} className="fontStyle-sm">
-                                                    {innerLabel}
+                                                  <a href={link} className="fontStyle-sm">
+                                                    {label}
                                                   </a>
                                                 )
-                                              })}
-                                            </div>
-                                          )
-                                        case 'link': {
-                                          const Alink = () => {
-                                            return (
-                                              <a href={link} className="fontStyle-sm">
-                                                {label}
-                                              </a>
-                                            )
+                                              }
+                                              if (layout === 'horizontal') {
+                                                const lClass = idx + 1 < contentLength ? 'after-content after:mr-2 after:ml-2' : ''
+                                                return (
+                                                  <div className={lClass} data-content-after="-">
+                                                    <Alink />
+                                                  </div>
+                                                )
+                                              }
+                                              if (layout === 'vertical') {
+                                                return <Alink />
+                                              }
+                                              return null
+                                            }
+                                            case 'button':
+                                              return (
+                                                <div className="dark">
+                                                  <Button
+                                                    label={label}
+                                                    link={link}
+                                                    type="primary"
+                                                    icon="none"
+                                                    size="default"
+                                                    forceDark={true}
+                                                    className="lg:ml-6 lg:first:ml-0"
+                                                  />
+                                                </div>
+                                              )
+                                            case 'NavigationDynamicList':
+                                              if (dataSource === 'language') {
+                                                return (
+                                                  <NavItem
+                                                    styles="default"
+                                                    popup="elevated"
+                                                    padding={false}
+                                                    label={locales.current ? locales.current : 'English'}
+                                                    listNavContent={locales.content}
+                                                    dropdownTop={true}
+                                                  />
+                                                )
+                                              }
+                                              console.error('unrecognized dataSource', dataSource, typeof dataSource)
+                                              return null
+                                            case 'NavigationLogo':
+                                              if (innerContent.image) {
+                                                return (
+                                                  <div className="">
+                                                    <Media media={innerContent.image} className="h-8 w-auto" />
+                                                  </div>
+                                                )
+                                              }
+                                              return <div>Add business logo here</div>
+                                            default:
+                                              console.error('Unrecognized footer type', type)
+                                              return null
                                           }
-                                          if (layout === 'horizontal') {
-                                            const lClass = idx + 1 < contentLength ? 'after-content after:mr-2 after:ml-2' : ''
-                                            return (
-                                              <div className={lClass} data-content-after="-">
-                                                <Alink />
-                                              </div>
-                                            )
-                                          }
-                                          if (layout === 'vertical') {
-                                            return <Alink />
-                                          }
-                                          return null
-                                        }
-                                        case 'button':
-                                          return (
-                                            <div className="dark">
-                                              <Button
-                                                label={label}
-                                                link={link}
-                                                type="primary"
-                                                icon="none"
-                                                size="default"
-                                                forceDark={true}
-                                                className="lg:ml-6 lg:first:ml-0"
-                                              />
-                                            </div>
-                                          )
-                                        case 'NavigationDynamicList':
-                                          if (dataSource === 'language') {
-                                            return (
-                                              <NavItem
-                                                styles="default"
-                                                popup="elevated"
-                                                padding={false}
-                                                label={locales.current ? locales.current : 'English'}
-                                                listNavContent={locales.content}
-                                                dropdownTop={true}
-                                              />
-                                            )
-                                          }
-                                          console.error('unrecognized dataSource', dataSource, typeof dataSource)
-                                          return null
-                                        case 'NavigationLogo':
-                                          return (
-                                            <div className="">
-                                              <Media media={innerContent.image} className="h-8 w-auto" />
-                                            </div>
-                                          )
-                                        default:
-                                          console.error('Unrecognized footer type', type)
-                                          return null
-                                      }
-                                    })()}
-                                  </div>
-                                )
-                              })}
-                          </div>
-                        )
-                      })}
-                  </div>
-                )
-              })}
+                                        })()}
+                                      </div>
+                                    )
+                                  })
+                                : null}
+                            </div>
+                          )
+                        })}
+                    </div>
+                  )
+                })
+              : null}
           </section>
         )
       })}
