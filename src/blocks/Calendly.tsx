a5619496958d706855c51471ae2737c2c1a34f7f
Shawn Inder <shawninder@gmail.com>
2021-10-22 17:47:37 -0400

    Revert to something closer to the original code snippet

--- src/blocks/Calendly.tsx
+++ src/blocks/Calendly.tsx
@@ -6,7 +6,11 @@ export interface CalendlyProps {}
 export const Calendly: FC<CalendlyProps> = ({}) => {
   return (
     <>
-      <div className={cn('calendly-inline-widget w-3/4 h-96 border-3')} data-url="https://calendly.com/stream-1/30min" />
+      <div
+        className={cn('calendly-inline-widget')}
+        style={{minWidth: '320px', height: '630px'}}
+        data-url="https://calendly.com/stream-1/30min"
+      />
       <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
     </>
   )
