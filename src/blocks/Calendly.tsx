783fbfe2d5053834acea987494c5d5c4ccc1bbc1
Shawn Inder <shawninder@gmail.com>
2021-10-23 12:28:15 -0400

    Import script in `<head>` section to fix Calendly widget

--- src/blocks/Calendly.tsx
+++ src/blocks/Calendly.tsx
@@ -1,17 +1,24 @@
-import React, {FC} from 'react'
+import React, {FC, useEffect} from 'react'
 import cn from 'classnames'
 
 export interface CalendlyProps {}
 
-export const Calendly: FC<CalendlyProps> = ({}) => {
+export const Calendly: FC<CalendlyProps> = () => {
+  useEffect(() => {
+    const head = document.querySelector('head')
+    const script = document.createElement('script')
+    script.setAttribute('src', 'https://assets.calendly.com/assets/external/widget.js')
+    script.setAttribute('async', 'true')
+    head.appendChild(script)
+    return () => {
+      head.removeChild(script)
+    }
+  }, [])
   return (
-    <>
-      <div
-        className={cn('calendly-inline-widget')}
-        style={{minWidth: '320px', height: '630px'}}
-        data-url="https://calendly.com/stream-1/30min"
-      />
-      <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
-    </>
+    <div
+      className={cn('calendly-inline-widget')}
+      style={{minWidth: '320px', height: '630px'}}
+      data-url="https://calendly.com/stream-1/30min"
+    />
   )
 }
