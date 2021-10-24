137c52185879f45fb83431e6523178a93ff326b3
Shawn Inder <shawninder@gmail.com>
2021-10-24 17:20:57 -0400

    Debug version of Collection component

--- src/collections/Collection.tsx
+++ src/collections/Collection.tsx
@@ -1,9 +1,10 @@
 import React, {FC, HTMLAttributes} from 'react'
 
 export interface CollectionProps extends HTMLAttributes<HTMLDivElement> {
-  dataSource: string
+  items: any[]
 }
 
-export const Collection: FC<CollectionProps> = ({dataSource}) => {
-  return <p>Collection. Data source: {dataSource}</p>
+export const Collection: FC<CollectionProps> = ({items} = {items: []}) => {
+  const finalItems = items || []
+  return <pre>{JSON.stringify(finalItems, null, 2)}</pre>
 }
