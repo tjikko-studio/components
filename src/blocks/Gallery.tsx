5b9b1b10b282ab074ef6f1de677f1788c85e78ed
Shawn Inder <shawninder@gmail.com>
2021-10-27 14:34:50 -0400

    `[]` as images default value instead of `null`

--- src/blocks/Gallery.tsx
+++ src/blocks/Gallery.tsx
@@ -35,7 +35,7 @@ export interface GalleryProps extends HTMLAttributes<HTMLDivElement> {
 /**
  * Primary UI component for user interaction
  */
-export const Gallery: FC<GalleryProps> = ({images = null, className = '', caption = ''}) => {
+export const Gallery: FC<GalleryProps> = ({images = [], className = '', caption = ''}) => {
   let gridLayout = ''
   switch (images.length) {
     case 2:
@@ -51,21 +51,19 @@ export const Gallery: FC<GalleryProps> = ({images = null, className = '', captio
   return (
     <figure role="group" className={cn(className)}>
       <div className={cn('grid gap-2', gridLayout)}>
-        {images
-          ? images.map((image) => {
-              return (
-                <Media
-                  media={image}
-                  autoplay={false}
-                  muted={false}
-                  controls={false}
-                  loop={false}
-                  className={'relative rounded-lg shadow-xl w-full h-full overflow-hidden'}
-                  gallery={true}
-                />
-              )
-            })
-          : null}
+        {images.map((image) => {
+          return (
+            <Media
+              media={image}
+              autoplay={false}
+              muted={false}
+              controls={false}
+              loop={false}
+              className={'relative rounded-lg shadow-xl w-full h-full overflow-hidden'}
+              gallery={true}
+            />
+          )
+        })}
       </div>
       <figcaption
         className="fontStyle-sm bg-opacity-60 text-gray:500 dark:text-gray-400 mt-4 relative"
