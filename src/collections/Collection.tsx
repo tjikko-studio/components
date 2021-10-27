6cc8319accc9c5f3e3cb613ab86e2191409ad59f
Catherine <catherine.desrochers@gmail.com>
2021-10-27 16:41:13 -0400

    Success stories uses the tertiary layout, I replaced it

--- src/collections/Collection.tsx
+++ src/collections/Collection.tsx
@@ -2,6 +2,7 @@ import React, {FC, HTMLAttributes} from 'react'
 
 import {Gallery} from '../blocks/Gallery'
 import {Heading} from '../blocks/Heading'
+import {Tertiary} from '../blocks/Tertiary'
 import {Button} from '../Button'
 import {ImageProps, Media} from '../parts/Media'
 
@@ -25,17 +26,13 @@ const SuccessStoriesCollection: FC<CollectionProps> = ({items} = {items: []}) =>
   return (
     <div className="flex flex-wrap m-8">
       {items?.map((item) => (
-        <div key={item.id} className="flex w-full p-8 lg:w-1/2">
-          <div className="flex flex-col w-1/2 mr-4">
-            <Heading text={item.content.title} className="pb-4" />
-            <p className="pb-4 text-left">{item.content.description}</p>
-            <Button type="tertiary" link={item.url} label="Learn more" />
-          </div>
-
-          <div className="w-1/2">
-            <Media media={item.content.images[0]} className="h-full rounded-xl" wrapperClassName="h-full" />
-          </div>
-        </div>
+        <Tertiary
+          key={item.id}
+          image={item.content.images[0]}
+          imagePosition="right"
+          title={item.content.title}
+          body={item.content.description}
+        />
       ))}
     </div>
   )
