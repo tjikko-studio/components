0e389452af698b316cd20f5bdaae75c1718976cd
Shawn Inder <shawninder@gmail.com>
2021-10-29 14:26:58 -0400

    Revert useless modification

--- src/blocks/ButtonsGroup.tsx
+++ src/blocks/ButtonsGroup.tsx
@@ -31,14 +31,12 @@ const DEFAULT_COLUMN_INDEX = 1
 /**
  * Primary UI component for user interaction
  */
-export const ButtonsGroup: FC<ButtonsGroupProps> = (
-  {buttons, className, columnIndex, fullWidth} = {
-    buttons: [],
-    className: '',
-    columnIndex: DEFAULT_COLUMN_INDEX,
-    fullWidth: false
-  }
-) => {
+export const ButtonsGroup: FC<ButtonsGroupProps> = ({
+  buttons = [],
+  className = '',
+  columnIndex = DEFAULT_COLUMN_INDEX,
+  fullWidth = false
+}) => {
   return (
     <div
       className={cn('inline-flex items-center space-x-4', fullWidth ? 'w-full' : 'w-max', className)}
