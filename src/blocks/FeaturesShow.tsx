5fec4f9fce8576bd9fd7c080c23da506a2becacd
Shawn Inder <shawninder@gmail.com>
2021-10-29 15:08:41 -0400

    WIP got code from Codepen
    Just need to figure out why it's not working + tweak

--- src/blocks/FeaturesShow.tsx
+++ src/blocks/FeaturesShow.tsx
@@ -1,8 +1,8 @@
-import React, {FC, useEffect, useState} from 'react'
+import React, {FC, useEffect, useRef} from 'react'
 import cn from 'classnames'
 
 import getComponent from '../../utilities/getComponent'
-import {ImageProps, Media} from '../parts/Media'
+import {ImageProps} from '../parts/Media'
 
 import {BlockProps, ContentPosition} from '../../shared/types'
 
@@ -14,6 +14,8 @@ type FeaturesShowItemBox = {
    * Content Position
    */
   position?: ContentPosition
+  idx?: number
+  totalNbBoxes?: number
 }
 
 type FeaturesShowItem = {
@@ -58,45 +60,166 @@ function extractCombo(thing: string) {
   return thing ? thing.split('|') : [null, null]
 }
 
-const FeatureShowItem: FC<{item: FeaturesShowItem}> = ({item}) => {
-  const [currentBoxIdx, setCurrentBoxIdx] = useState(0)
-  const totalBoxes = item.info_boxes?.length || 0
+const growthFactor = 2
+
+type Caps = [number, number]
+type Easing = (val: number) => number
+
+function cap(val: number, [lower, upper]: Caps) {
+  const delta = upper - lower
+  return val * delta + lower
+}
+
+function observerArgs(
+  cb: Function,
+  {
+    steps = 2,
+    easing = (val) => val,
+    caps = [0, 1]
+  }: {
+    steps?: number
+    easing?: Easing
+    caps?: Caps
+  } = {}
+): [([entry]: IntersectionObserverEntry[]) => void, IntersectionObserverInit] {
+  const [min, max] = [0, 1]
+  const delta = max - min
+  const threshold: number[] = []
+  for (let curr = min; curr <= max; curr = curr + delta / (steps - 1)) {
+    threshold.push(Math.round(1000 * curr) / 1000)
+  }
+
+  return [
+    (entries: IntersectionObserverEntry[]) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const elem = entry.target
+          threshold.some((ratio) => {
+            if (entry.intersectionRatio <= ratio) {
+              const eased = easing(ratio)
+              const val = cap(eased, caps)
+              cb({elem, val})
+              return true
+            }
+            return false
+          })
+        }
+      })
+    },
+    {
+      threshold
+    }
+  ]
+}
+
+function getOpacity(elem: HTMLElement, idx: number, length: number) {
+  const viewportHeight = window.innerHeight
+  const fraction = viewportHeight / length
+  const threshold = (idx * viewportHeight) / length + fraction / 2
+  const {top, height} = elem.getBoundingClientRect()
+  const distance = threshold - top
+  if (distance < 0) {
+    return '0'
+  } else if (distance >= height) {
+    return '0'
+  } else {
+    return `${distance / (height / growthFactor)}`
+  }
+}
+
+type ObserverArgsCallback = {elem: HTMLElement; val: number}
+
+const InfoBox: FC<FeaturesShowItemBox> = ({title, body, position, idx, totalNbBoxes}) => {
+  const infoBoxContainerRef = useRef(null)
+  const [verPosVal, horPosVal] = extractCombo(position)
+  const verPos = getVerPos(verPosVal)
+  const horPos = getHorPos(horPosVal)
 
   useEffect(() => {
-    let idx = 0
-    const intervalId = setInterval(() => {
-      setCurrentBoxIdx(idx % totalBoxes)
-      idx++
-    }, 2 * 1000)
-
-    return () => {
-      clearInterval(intervalId)
+    if (infoBoxContainerRef.current) {
+      infoBoxContainerRef.current.style.opacity = 0
+      const popupContainerObserver = new IntersectionObserver(
+        ...observerArgs(
+          ({elem, val}: ObserverArgsCallback) => {
+            function alterOpacity() {
+              elem.style.opacity = getOpacity(elem, idx, totalNbBoxes)
+            }
+            alterOpacity()
+            if (val) {
+              window.addEventListener('scroll', alterOpacity, false)
+            } else {
+              window.removeEventListener('scroll', alterOpacity, false)
+            }
+          },
+          {
+            steps: 5
+          }
+        )
+      )
+      popupContainerObserver.observe(infoBoxContainerRef.current)
     }
-  }, [totalBoxes])
+  }, [infoBoxContainerRef, idx, totalNbBoxes])
+  return (
+    <div
+      className="relative flex items-start transition-opacity"
+      ref={infoBoxContainerRef}
+      style={{
+        height: `${(growthFactor * 100) / totalNbBoxes}vh`
+      }}
+    >
+      <div
+        key={idx}
+        className={cn(
+          'absolute z-20 bg-white rounded-xl shadow-xl w-1/3 p-8 transition-opacity duration-2000',
+          verPos,
+          horPos
+          // currentBoxIdx === idx ? 'opacity-100' : 'opacity-0'
+        )}
+      >
+        <div className="mb-4 fontStyle-2xl" dangerouslySetInnerHTML={{__html: title}} />
+        <div dangerouslySetInnerHTML={{__html: body}} />
+      </div>
+    </div>
+  )
+}
+
+const imgContainerObserver = new IntersectionObserver(
+  ...observerArgs(
+    ({elem, val}: ObserverArgsCallback) => {
+      const bgImg: HTMLElement = elem.querySelector('.bg-img')
+      bgImg.style.transform = `scale(${val})`
+    },
+    {
+      steps: 5,
+      caps: [0.8, 0.9]
+    }
+  )
+)
+
+const FeatureShowItem: FC<{item: FeaturesShowItem}> = ({item}) => {
+  const imgContainerRef = useRef(null)
+
+  useEffect(() => {
+    if (imgContainerRef.current) {
+      imgContainerObserver.observe(imgContainerRef.current)
+    }
+  }, [imgContainerRef])
 
   return (
     <div className="flex flex-col items-center mx-10 h-100vh">
       <div className="relative w-full px-8">
-        {item.image?.[0] && <Media media={item.image?.[0]} className="w-full h-80vh rounded-xl" />}
-        {item.info_boxes.map((box, idx) => {
-          const [verPosVal, horPosVal] = extractCombo(box.position)
-          const verPos = getVerPos(verPosVal)
-          const horPos = getHorPos(horPosVal)
-
-          return (
+        {item.image?.[0] && (
+          <div className="sticky t-0 h-100vh" ref={imgContainerRef}>
             <div
-              key={idx}
-              className={cn(
-                'absolute z-20 bg-white rounded-xl shadow-xl w-1/3 p-8 transition-opacity duration-2000',
-                verPos,
-                horPos,
-                currentBoxIdx === idx ? 'opacity-100' : 'opacity-0'
-              )}
-            >
-              <div className="mb-4 fontStyle-2xl" dangerouslySetInnerHTML={{__html: box.title}} />
-              <div dangerouslySetInnerHTML={{__html: box.body}} />
-            </div>
-          )
+              className="bg-img h-full w-full bg-center bg-cover rounded-3xl transition-transform duration-700"
+              style={{
+                backgroundImage: `url(${item.image?.[0].url})`
+              }}
+            />
+          </div>
+        )}
+        {item.info_boxes.map((box, idx, all) => {
+          return <InfoBox key={box.title} {...box} idx={idx} totalNbBoxes={all.length} />
         })}
       </div>
     </div>
