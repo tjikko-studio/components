c778fa4d2a349ec3e258d90cb3df15c9c3bf62ba
Shawn Inder <shawninder@gmail.com>
2021-11-10 17:58:49 -0400

    Dont convert to numbers by default

--- utilities/stringUtils.ts
+++ utilities/stringUtils.ts
@@ -1,7 +1,3 @@
 export default function extractCombo(thing: string, separ: string = '|'): (string | null)[] {
-  return thing
-    ? thing.split(separ).map(function (item: any) {
-        return Number(item) ? parseInt(item) : item
-      })
-    : [null, null]
+  return thing ? thing.split(separ) : [null, null]
 }
