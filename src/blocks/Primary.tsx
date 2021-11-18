eeafa930969b5a2d7f6eefe10b959188b169cb54
Shawn Inder <shawninder@gmail.com>
2021-11-18 00:29:09 -0500

    `buttons: ButtonProps[]` instead of `GroupButtonProps[]`

--- src/blocks/Primary.tsx
+++ src/blocks/Primary.tsx
@@ -1,7 +1,8 @@
 import React, {FC, HTMLAttributes} from 'react'
 import cn from 'classnames'
 
-import {ButtonsGroup, GroupButtonProps} from '../blocks/ButtonsGroup'
+import {ButtonsGroup} from '../blocks/ButtonsGroup'
+import {ButtonProps} from '../Button'
 import {ImageProps, Media} from '../parts/Media'
 
 export interface PrimaryProps extends HTMLAttributes<HTMLDivElement> {
@@ -51,7 +52,7 @@ export interface PrimaryProps extends HTMLAttributes<HTMLDivElement> {
   /**
    *  Block buttons list
    */
-  buttons?: GroupButtonProps[]
+  buttons?: ButtonProps[]
 }
 
 /**
@@ -94,13 +95,7 @@ export const Primary: FC<PrimaryProps> = ({
             className="rounded-lg shadow-xl w-full h-full"
             wrapperClassName={'flex-1'}
           />
-          <div
-            className={cn(
-              'flex flex-col flex-1',
-              {'justify-center': textPositionVertical === 'center'},
-              className
-            )}
-          >
+          <div className={cn('flex flex-col flex-1', {'justify-center': textPositionVertical === 'center'}, className)}>
             <h2 className={cn('break-words', titleSize)}>{title}</h2>
             <p className="mt-2" dangerouslySetInnerHTML={{__html: body}} />
             {Object.keys(buttons).length ? <ButtonsGroup key={JSON.stringify(buttons)} buttons={buttons} className="gap-x-4 mt-4" /> : null}
