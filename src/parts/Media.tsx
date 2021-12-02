15a197316c6e0d893edfa6d49fd348e99c088e96
Catherine <catherine.desrochers@gmail.com>
2021-12-02 18:11:12 -0500

    Better placeholder for the media

--- src/parts/Media.tsx
+++ src/parts/Media.tsx
@@ -157,8 +157,8 @@ export const Media: FC<GenericMediaProps> = ({
   ) : media && media.type === 'image' ? (
     <MediaImage key={media.url} {...media} ratio={ratio} className={className} fit={fit} mediaClasses={mediaClasses} />
   ) : (
-    <div className={cn('max-w-full overflow-hidden', className, mediaClasses, ratio && `ratio-${ratio}`)}>
-      <div className="w-full h-full justify-center flex items-center p-4 bg-gray-300 text-gray-800 opacity-50">
+    <div className={cn('flex max-w-full overflow-hidden', className, mediaClasses, ratio && `ratio-${ratio}`)}>
+      <div className="w-full justify-center flex items-center p-4 bg-gray-300 text-gray-800 opacity-50">
         <MediaIcon className="w-8 h-8" fit={fit} />
       </div>
     </div>
