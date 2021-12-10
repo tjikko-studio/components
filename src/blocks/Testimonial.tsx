6b1c96d606ff8d359f03b0dd3a1eddd313521a5c
Shawn Inder <shawninder@gmail.com>
2021-12-10 15:55:07 -0500

    Center testimonial image

--- src/blocks/Testimonial.tsx
+++ src/blocks/Testimonial.tsx
@@ -22,7 +22,7 @@ export interface TestimonialProps extends HTMLAttributes<HTMLDivElement> {
  */
 export const Testimonial: FC<TestimonialProps> = ({body = '', image = {}, title = ''}) => {
   return (
-    <div className="flex flex-col text-gray-700 dark:text-gray-200">
+    <div className="flex flex-col items-center text-gray-700 dark:text-gray-200">
       <p className="fontStyle-lg italic text-center">“{body}”</p>
       <Media media={image} className="w-28 h-28 mxl:x-auto rounded-full mt-4" ratio="1/1" />
       <p className="fontStyle-xs uppercase text-center mt-6">{title}</p>
