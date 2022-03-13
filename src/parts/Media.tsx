f5cbe4e3c05a75ca93792b1c459b9b45f97b17e7
Shawn Inder <shawninder@gmail.com>
2022-03-12 19:06:39 -0500

    Use object-contain instead of object-cover when ratio is `unset`
    fixes the safari image cropping bug

--- src/parts/Media.tsx
+++ src/parts/Media.tsx
@@ -39,7 +39,7 @@ export const MediaImage = ({id, url, ratio, mediaClasses, className, alt = '', i
         <img
           src={url}
           alt={alt && alt !== '' ? alt : parsedInfos?.alt}
-          className={cn('h-full', ratio && 'object-cover', fit ? 'w-auto' : 'w-full', mediaClasses)}
+          className={cn('h-full', ratio && ratio !== 'unset' ? 'object-cover' : 'object-contain', fit ? 'w-auto' : 'w-full', mediaClasses)}
         />
       )}
       {parsedInfos?.caption && <FigCaption caption={parsedInfos?.caption} />}
