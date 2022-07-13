da21596149407d06e11030c30cc8eeb2265c1423
Shawn Inder <shawninder@gmail.com>
2022-07-13 14:00:22 -0400

    Sanitize link URLs (remove secondary domains, use relative paths)

--- utilities/parseLink.tsx
+++ utilities/parseLink.tsx
@@ -13,10 +13,29 @@ export default function parseLink(link: LinkObject): ParsedLink {
       } else {
         parsed.url = typeof link.value === 'string' ? link.value : link.value[0]?.url
       }
+      parsed.url = sanitizeUrl(parsed.url)
     }
     if (link?.popup === true) {
       parsed.target = '_blank'
     }
   }
   return parsed
+}
+
+const defaultLocale = 'en'
+const secondaryDomains = ['https://smpx-admin.tjikko-studio.a2hosted.com', 'http://127.0.0.1:8000']
+const primaryDomain = 'https://smartpixel.com'
+
+function sanitizeUrl(url: string) {
+  // Use primary domain
+  secondaryDomains.forEach((domain) => {
+    if (url.substring(0, domain.length) === domain) {
+      url = url.replace(domain, primaryDomain)
+    }
+  })
+  if (url.substring(0, primaryDomain.length) === primaryDomain) {
+    url = url.replace(`/${defaultLocale}/`, '/') // Remove default language prefix
+    url = url.replace(primaryDomain, '') // prefer relative paths
+  }
+  return url
 }
